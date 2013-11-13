d1895c514bdc21eb17edcc50fc9bc16d9ddc7785
Avishai Ish-Shalom <avishai@fewbytes.com>
2013-11-13 11:41:47 +0200

    Added missing install recipe

---
+++ recipes/install.rb
@@ -0,0 +1,16 @@
+# this recipe installs jmxterm jar in node['jmxterm']['dest']
+# Please note that java will not be installed by this recipe. This is intentional
+
+directory :File.dirname(node['jmxterm']['dest']) do
+  mode 00755
+  recursive true
+end
+
+remote_file node['jmxterm']['dest'] do
+  owner "tomcat6"
+    group "tomcat6"
+    mode 00644
+    source node['jmxterm']['url']
+    checksum node['jmxterm']['checksum']
+    action :create_if_missing
+end
