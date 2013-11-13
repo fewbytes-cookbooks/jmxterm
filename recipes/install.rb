6526d6eea95547d1a5c270a1e6a4488c3a923b8f
Avishai Ish-Shalom <avishai@fewbytes.com>
2013-11-13 12:02:29 +0200

    fixed typo

--- recipes/install.rb
+++ recipes/install.rb
@@ -1,7 +1,7 @@
 # this recipe installs jmxterm jar in node['jmxterm']['dest']
 # Please note that java will not be installed by this recipe. This is intentional
 
-directory :File.dirname(node['jmxterm']['dest']) do
+directory ::File.dirname(node['jmxterm']['dest']) do
   mode 00755
   recursive true
 end
