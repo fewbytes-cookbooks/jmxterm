e48dea76155d4d92151da1b265735d95d60af9e0
Avishai Ish-Shalom <avishai@fewbytes.com>
2013-12-29 15:35:34 +0200

    Added support for user/password

--- providers/jmx_exec.rb
+++ providers/jmx_exec.rb
@@ -18,9 +18,14 @@ action :run do
 		new_resource.arguments
 	end
 
+	auth_opts = if new_resource.user and new_resource.password
+		"-u #{new_resource.user} -p #{new_resource.password}"
+	else
+		""
+	end
 	# Chef::Log.info("Simple Test #{new_resource.name}")
 	execute new_resource.name do
-		command "printf 'mbean #{new_resource.mbean}\n#{new_resource.operation} #{args}\n' | #{node['java']['java_home']['java']} -jar #{node['jmxterm']['dest']} -l #{new_resource.host}:#{new_resource.port}"
+		command "printf 'mbean #{new_resource.mbean}\nrun #{new_resource.operation} #{args}\n' | #{node['java']['java_home']['java']} -jar #{node['jmxterm']['dest']} -l #{new_resource.host}:#{new_resource.port} #{auth_opts}"
 		action :run
 	end
 end
--- resources/jmx_exec.rb
+++ resources/jmx_exec.rb
@@ -2,9 +2,9 @@ actions :run
 default_action :run
 
 attribute :mbean,		  kind_of: String, name_attribute: true, required: true
-attribute :operation, kind_of: String,	required: true, default: 'run'
-attribute :arguments, kind_of: [Array, String, Numeric]
-attribute :port,		  kind_of: String,	default: '9000'
+attribute :operation,     kind_of: String, required: true
+attribute :arguments,     kind_of: [Array, String, Numeric]
+attribute :port,		  kind_of: String, default: '9000'
 attribute :host,		  kind_of: String, default: 'localhost'
-attribute :user,		  kind_of: String,	default: nil
-attribute :password,	kind_of: String,	default: nil
+attribute :user,		  kind_of: String, default: nil
+attribute :password,	  kind_of: String, default: nil
